1718bf52dc7423b3d8c0e08807decfdf50f8fd60
Aman Singh <amankumarkvm4@gmail.com>
2026-05-23 22:56:44 +0530

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Aman Singh")
+# print("Aman Singh")
+
+import tweepy
+import pandas as pd
+import json
+import datetime as datetime
+import s3fs
+
+access_key = ""
+access_secret = ""
+consumer_key = ""
+consumer_secret = ""
