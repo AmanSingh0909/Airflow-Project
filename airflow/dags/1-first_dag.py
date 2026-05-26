6b5538126ade304fe7e2a61d2efbfd2c7415bb31
Aman Singh <amankumarkvm4@gmail.com>
2026-05-26 22:52:55 +0530

    trail dag

---
+++ airflow/dags/1-first_dag.py
@@ -0,0 +1,29 @@
+from airflow.sdk import dag, task
+
+@dag
+def first_dag():
+
+    @task.python
+    def first_task():
+        print("This is the first task")
+
+    @task.python
+    def secound_task():
+        print("This is the secound task")
+
+    @task.python
+    def third_task():
+        print("This is the third task. Dag is complete")
+
+
+    # Defining task dependencies
+
+    first = first_task()
+    secound = secound_task()
+    third = third_task()
+
+    first >> secound >> third
+
+
+# instantiating the DAG
+first_dag()
